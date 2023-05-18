74ac1ce5a942a9a49a5aa3020885e64a1258d168
tailorraj <tailorraj111@gmail.com>
2023-05-18 10:41:55 +0000

    Sales Report Completed With Financial Year Selection and Month bifurcation filters

--- mani_sales_customization/mani_sales_customization/report/sales_collection_target/sales_collection_target.js
+++ mani_sales_customization/mani_sales_customization/report/sales_collection_target/sales_collection_target.js
@@ -5,20 +5,30 @@
 frappe.query_reports["Sales Collection Target"] = {
 	"filters": [
 		{
-			"fieldname":"sales_person",
-			"label": __("Sales Person"),
-			"fieldtype": "Link",
-			"options": "Sales Person",
-			"reqd": 1,
-			"width": "100px"
+			fieldname:"company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company")
 		},
 		{
-			"fieldname":"year",
-			"label": __("Year"),
-			"fieldtype": "Select",
-			"options": "2022\n2023\n2024\n2025\n2026\n2027",
-			"reqd": 1,
-			"width": "100px"
+			fieldname: "fiscal_year",
+			label: __("Fiscal Year"),
+			fieldtype: "Link",
+			options: "Fiscal Year",
+			default: frappe.sys_defaults.fiscal_year
+		},
+		{
+			fieldname: "period",
+			label: __("Period"),
+			fieldtype: "Select",
+			options: [
+				{ "value": "Monthly", "label": __("Monthly") },
+				{ "value": "Quarterly", "label": __("Quarterly") },
+				{ "value": "Half-Yearly", "label": __("Half-Yearly") },
+				{ "value": "Yearly", "label": __("Yearly") }
+			],
+			default: "Monthly"
 		},
 	]
 };
--- mani_sales_customization/mani_sales_customization/report/sales_collection_target/sales_collection_target.py
+++ mani_sales_customization/mani_sales_customization/report/sales_collection_target/sales_collection_target.py
@@ -3,160 +3,401 @@
 
 import frappe
 from frappe import _
+from erpnext.accounts.report.financial_statements import get_period_list
+from erpnext.accounts.utils import get_fiscal_year
+from collections import defaultdict
+from datetime import datetime,date
 
 
 def execute(filters=None):
-	columns, data = get_columns(filters), get_data(filters)
+	period_list = get_period_list(filters.fiscal_year, filters.fiscal_year, '', '',
+		'Fiscal Year', filters.period, company=filters.company)
+
+	columns, data = get_data_column(filters , period_list), get_data(filters , period_list)
 	return columns, data
 
-
-def get_columns(filters):
-	columns = [{
-		"fieldname": "cost_center",
-		"label": "Cost Center",
-		"type":"Data",
-		"width":"80px"
-	}]
-        
-	columns = columns + get_month_labels(filters.get('year'))
-	# columns = [
-	# _("Null Column") + "Data:150",
-	# _("Total Outstanding") + "Data:150",
-	# _("March(Target)") + "Data:150",
-	# _("March(Achieved)") + "Data:150",
-	# _("March(Variance)") + "Data:150"]
-	# frappe.throw(str(columns))
-
+def get_data_column(filters , period_list):
 	
+	columns = get_columns(filters, period_list)
 	return columns
 
-def get_data(filters):
+def get_columns(filters, period_list):
+	fieldtype, options = "Currency", "currency"
+
+	columns = [
+	{
+	"fieldname": "cost_center",
+	"label": _("Cost Center"),
+	"fieldtype": "Link",
+	"options": "Cost Center",
+	"width": 150
+	},
+	{
+	"fieldname": "sales_person",
+	"label": _("Sales Person"),
+	"fieldtype": "Link",
+	"options": "Sales Person",
+	"width": 150
+	}]
+	
+	for period in period_list:
+		target_key = 'target_{}'.format(period.key)
+		variance_key = 'variance_{}'.format(period.key)
+
+		columns.extend([{
+			"fieldname": target_key,
+			"label": _("Target ({})").format(period.label),
+			"fieldtype": fieldtype,
+			"options": options,
+			"width": 150
+		}, {
+			"fieldname": period.key,
+			"label": _("Achieved ({})").format(period.label),
+			"fieldtype": fieldtype,
+			"options": options,
+			"width": 150
+		}, {
+			"fieldname": variance_key,
+			"label": _("Variance ({})").format(period.label),
+			"fieldtype": fieldtype,
+			"options": options,
+			"width": 150
+		}])
+
+	columns.extend([{
+		"fieldname": "total_target",
+		"label": _("Total Target"),
+		"fieldtype": fieldtype,
+		"options": options,
+		"width": 150
+	}, {
+		"fieldname": "total_achieved",
+		"label": _("Total Achieved"),
+		"fieldtype": fieldtype,
+		"options": options,
+		"width": 150
+	}, {
+		"fieldname": "total_variance",
+		"label": _("Total Variance"),
+		"fieldtype": fieldtype,
+		"options": options,
+		"width": 150
+	}])
+
+	return columns
+
+def get_data(filters , period_list):
+	fiscal_year = get_fiscal_year(fiscal_year=filters.get("fiscal_year"), as_dict=1)
+	from_date,to_date = fiscal_year.year_start_date, fiscal_year.year_end_date
+	# frappe.msgprint(str(from_date))
+	# frappe.msgprint(str(to_date))
 	sales_data = frappe.db.sql("""
 	select 
-
-	sum(ps.payment_amount) as sales_target,st.sales_person,si.cost_center,month(si.posting_date) as month,year(si.posting_date) as year 
-
+	sum(ps.payment_amount) as sales_target,si.sales_executive,si.cost_center,month(ps.due_date) as month,year(ps.due_date) as year 
 	from 
-
-	`tabSales Invoice` si inner join `tabPayment Schedule` ps on si.name = ps.parent 
-	left join `tabSales Team` st on st.parent = si.name
-
-	where si.docstatus = 1 and st.sales_person = %(sales_person)s and year(si.posting_date) = %(year)s
-
-	group by si.cost_center,month(si.posting_date),year(si.posting_date)
-
-	order by month(si.posting_date),year(si.posting_date)
+	`tabSales Invoice` si inner join `tabPayment Schedule` ps on si.name = ps.parent
+	where si.docstatus = 1 and ps.due_date between %(from_date)s and %(to_date)s
+	group by si.cost_center,month(ps.due_date),year(ps.due_date),si.sales_executive
 	""",{
-		"sales_person":filters.get('sales_person'),
-        "year":filters.get('year')
+		"from_date":from_date,
+		"to_date":to_date
 	},as_dict=1)
 
 	payment_data = frappe.db.sql("""
-	select sum(per.allocated_amount) as paid_amount,st.sales_person,pe.cost_center,month(pe.posting_date) as month,year(pe.posting_date) as year 
-
+	select sum(per.allocated_amount) as paid_amount,si.sales_executive,si.cost_center,month(pe.posting_date) as month,year(pe.posting_date) as year
 	from 
-
 	`tabPayment Entry Reference` per inner join `tabPayment Entry`  pe on pe.name = per.parent 
-	left join `tabSales Team` st on st.parent = per.reference_name
-
-	where 
-
-	per.reference_doctype = "Sales Invoice" and pe.docstatus = 1 and st.sales_person = %(sales_person)s and year(pe.posting_date) = %(year)s
-
+	left join `tabSales Invoice` si on si.name = per.reference_name
+	where per.reference_doctype = "Sales Invoice" and pe.docstatus = 1 and pe.posting_date between %(from_date)s and %(to_date)s
 	group by 
-
-	month(pe.posting_date),year(pe.posting_date),pe.cost_center
-
+	month(pe.posting_date),year(pe.posting_date),si.cost_center,si.sales_executive
 	""",({
-		"sales_person":filters.get('sales_person'),
-        "year":filters.get('year')
+		"from_date":from_date,
+		"to_date":to_date
 	}),as_dict=1)
+
+	grouped_data = aggregate_data(filters.get('period'), sales_data, payment_data, period_list)
+	return grouped_data
+
+def aggregate_data(period, sales_data, payment_data,period_list):
+	period_dict = {'Monthly': 1, 'Quarterly': 3, 'Half-Yearly': 6, 'Yearly': 12}
+	period_value = period_dict[period]
+
+	agg_data = defaultdict(lambda: defaultdict(float))
+
+	for p in period_list:
+		payments_ = []
+		sales_ = []
+		date_time = datetime.now()
+
+		if period_value == 1:
+			date_time = datetime.strptime(p.key, "%b_%Y")
+			sales_ = list(filter(lambda x : x['month'] == date_time.month and x['year'] == date_time.year, sales_data))
+			payments_ = list(filter(lambda x : x['month'] == date_time.month and x['year'] == date_time.year, payment_data))
+		elif period_value == 12:
+			sales_ = list(filter(lambda x : filter_year(x,p), sales_data))
+			payments_ = list(filter(lambda x : filter_year(x,p), payment_data))
+		else:
+			sales_ = list(filter(lambda x : filter_month_year(x,p), sales_data))
+			payments_ = list(filter(lambda x : filter_month_year(x,p), payment_data))
+
+		for d in sales_:
+			key = (d['sales_executive'], d['cost_center'], p.key)
+			agg_data[key]['sales_target'] += d['sales_target']
+
+		for d in payments_:
+			key = (d['sales_executive'], d['cost_center'], p.key)
+			agg_data[key]['paid_amount'] += d['paid_amount']
+
+	result = []
+	total_target = 0
+	total_achieved = 0
+	total_variance = 0
+
+	new_agg_data = defaultdict(lambda: defaultdict(list))
+	new_agg_total_target = defaultdict(lambda: defaultdict(float))
+	new_agg_total_archived = defaultdict(lambda: defaultdict(float))
+	new_agg_total_variance = defaultdict(lambda: defaultdict(float))
+
+	for key, value in agg_data.items():
+		sales_person, cost_center, period = key
+		sales_target = value['sales_target']
+		paid_amount = value['paid_amount']
+		variance = sales_target - paid_amount
+
+		total_target += sales_target
+		total_achieved += paid_amount
+		total_variance += variance
+
+		target_key = 'target_{}'.format(period)
+		variance_key = 'variance_{}'.format(period)
+		period_key = period
+
+		new_key = (sales_person,cost_center)
+
+		if not new_agg_data[new_key]:
+			new_agg_data[new_key] = []
+
+		new_agg_data[new_key].append({
+			period_key : paid_amount,
+			target_key : sales_target,
+			variance_key : variance
+		})
+
+		new_agg_total_target[new_key]["sales_target"] += sales_target
+		new_agg_total_archived[new_key]["paid_amount"] += paid_amount
+		new_agg_total_variance[new_key]["variance"] += variance
+
+	for key, value in new_agg_data.items():
+		sales_person,cost_center = key
+
+		single_result = {}
+		single_result["cost_center"] = cost_center
+		single_result["sales_person"] = sales_person
+		for items in value:
+			single_result.update(items)
+
+		single_result.update({
+			"total_target" : new_agg_total_target[key]["sales_target"],
+			"total_achieved" : new_agg_total_archived[key]["paid_amount"],
+			"total_variance" : new_agg_total_variance[key]["variance"]
+		})
+
+		result.append(single_result)
+
+	return result
+
+def filter_month_year(x , p):
+	start_ = p.from_date
+	end_ = p.to_date
+
+	month = int(x['month'])
+	year = int(x['year'])
+
+	return month >= start_.month and year >= start_.year and month <= end_.month and year <= end_.year
+
+def filter_year(x , p):
+	start_ = p.from_date
+	end_ = p.to_date
+
+	return x['year'] >= start_.year and x['year'] <= end_.year
+
+
+# def get_columns(filters):
+# 	columns = [{
+# 		"fieldname": "cost_center",
+# 		"label": "Cost Center",
+# 		"type":"Data",
+# 		"width":"80px"
+# 	},
+# 	{
+# 		"fieldname": "sales_person",
+# 		"label": "Sales Person",
+# 		"type":"Link",
+# 		"options":"Sales Person",
+# 		"width":"80px"
+# 	}]
+# 	columns = columns + get_month_labels(filters.get('year'))
+
+# 	columns.extend(
+# 		[
+# 			{
+# 				"fieldname": "total_target",
+# 				"label": _("Total Target"),
+# 				"fieldtype": "Float",
+# 				"width": 150,
+# 			},
+# 			{
+# 				"fieldname": "total_achieved",
+# 				"label": _("Total Achieved"),
+# 				"fieldtype": "Float",
+# 				"width": 150,
+# 			},
+# 			{
+# 				"fieldname": "total_variance",
+# 				"label": _("Total Variance"),
+# 				"fieldtype": "Float",
+# 				"width": 150,
+# 			},
+# 		]
+# 	)
+# 	# columns = [
+# 	# _("Null Column") + "Data:150",
+# 	# _("Total Outstanding") + "Data:150",
+# 	# _("March(Target)") + "Data:150",
+# 	# _("March(Achieved)") + "Data:150",
+# 	# _("March(Variance)") + "Data:150"]
+# 	# frappe.throw(str(columns))
+
+	
+# 	return columns
+
+# def get_data(filters):
+# 	pass
+# 	sales_data = frappe.db.sql("""
+# 	select 
+
+# 	sum(ps.payment_amount) as sales_target,st.sales_person,si.cost_center,month(si.posting_date) as month,year(si.posting_date) as year 
+
+# 	from 
+
+# 	`tabSales Invoice` si inner join `tabPayment Schedule` ps on si.name = ps.parent 
+# 	left join `tabSales Team` st on st.parent = si.name
+
+# 	where si.docstatus = 1 and year(si.posting_date) = %(year)s
+
+# 	group by si.cost_center,month(si.posting_date),year(si.posting_date),st.sales_person
+
+# 	order by month(si.posting_date),year(si.posting_date)
+# 	""",{
+#         "year":filters.get('year')
+# 	},as_dict=1)
+
+# 	payment_data = frappe.db.sql("""
+# 	select sum(per.allocated_amount) as paid_amount,st.sales_person,pe.cost_center,month(pe.posting_date) as month,year(pe.posting_date) as year 
+
+# 	from 
+
+# 	`tabPayment Entry Reference` per inner join `tabPayment Entry`  pe on pe.name = per.parent 
+# 	left join `tabSales Team` st on st.parent = per.reference_name
+
+# 	where 
+
+# 	per.reference_doctype = "Sales Invoice" and pe.docstatus = 1 and year(pe.posting_date) = %(year)s
+
+# 	group by 
+
+# 	month(pe.posting_date),year(pe.posting_date),pe.cost_center,st.sales_person
+
+# 	""",({
+#         "year":filters.get('year')
+# 	}),as_dict=1)
 
 	# frappe.msgprint(str(sales_data))
 	# frappe.msgprint(str(payment_data))
-	res_dict = merge_sales_payment_dicts(sales_data,payment_data)
-	final_res = group_by_cost_center(res_dict)
-	# frappe.msgprint(str(final_res))
-	return final_res
+# 	res_dict = merge_sales_payment_dicts(sales_data,payment_data)
+# 	grouped_data = group_by_cost_center(res_dict)
+# 	frappe.msgprint(str(grouped_data))
+# 	return grouped_data
 
 
-def merge_sales_payment_dicts(sales_list, payment_list):
-    merged_list = []
+# def merge_sales_payment_dicts(sales_list, payment_list):
+#     merged_list = []
     
-    # Create a dictionary of sales records
-    sales_dict = {}
-    for sales_record in sales_list:
-        key = (sales_record["sales_person"], sales_record["cost_center"], sales_record["month"], sales_record["year"])
-        sales_dict[key] = sales_record
+#     # Create a dictionary of sales records
+#     sales_dict = {}
+#     for sales_record in sales_list:
+#         key = (sales_record["sales_person"], sales_record["cost_center"], sales_record["month"], sales_record["year"])
+#         sales_dict[key] = sales_record
     
-    # Merge the payment records with the sales records
-    for payment_record in payment_list:
-        key = (payment_record["sales_person"], payment_record["cost_center"], payment_record["month"], payment_record["year"])
-        merged_dict = {
-            "sales_target-{}-{}".format(payment_record["month"], payment_record["year"]): 0.0,
-            "paid_amount-{}-{}".format(payment_record["month"], payment_record["year"]): payment_record["paid_amount"],
-            "variance-{}-{}".format(payment_record["month"], payment_record["year"]): 0.0,
-            "sales_person": payment_record["sales_person"],
-            "cost_center": payment_record["cost_center"],
-        }
-        if key in sales_dict:
-            sales_record = sales_dict[key]
-            merged_dict["sales_target-{}-{}".format(payment_record["month"], payment_record["year"])] = sales_record["sales_target"]
-            merged_dict["variance-{}-{}".format(payment_record["month"], payment_record["year"])] = sales_record["sales_target"] - payment_record["paid_amount"]
-            del sales_dict[key]
-        else:
-            merged_dict["variance-{}-{}".format(payment_record["month"], payment_record["year"])] = -payment_record["paid_amount"]
-        merged_list.append(merged_dict)
+#     # Merge the payment records with the sales records
+#     for payment_record in payment_list:
+#         key = (payment_record["sales_person"], payment_record["cost_center"], payment_record["month"], payment_record["year"])
+#         merged_dict = {
+#             "sales_target-{}-{}".format(payment_record["month"], payment_record["year"]): 0.0,
+#             "paid_amount-{}-{}".format(payment_record["month"], payment_record["year"]): payment_record["paid_amount"],
+#             "variance-{}-{}".format(payment_record["month"], payment_record["year"]): 0.0,
+#             "sales_person": payment_record["sales_person"],
+#             "cost_center": payment_record["cost_center"],
+#         }
+#         if key in sales_dict:
+#             sales_record = sales_dict[key]
+#             merged_dict["sales_target-{}-{}".format(payment_record["month"], payment_record["year"])] = sales_record["sales_target"]
+#             merged_dict["variance-{}-{}".format(payment_record["month"], payment_record["year"])] = sales_record["sales_target"] - payment_record["paid_amount"]
+#             del sales_dict[key]
+#         else:
+#             merged_dict["variance-{}-{}".format(payment_record["month"], payment_record["year"])] = -payment_record["paid_amount"]
+#         merged_list.append(merged_dict)
     
-    # Add any remaining sales records to the merged list
-    for sales_record in sales_dict.values():
-        merged_dict = {
-            "sales_target-{}-{}".format(sales_record["month"], sales_record["year"]): sales_record["sales_target"],
-            "paid_amount-{}-{}".format(sales_record["month"], sales_record["year"]): 0.0,
-            "variance-{}-{}".format(sales_record["month"], sales_record["year"]): sales_record["sales_target"],
-            "sales_person": sales_record["sales_person"],
-            "cost_center": sales_record["cost_center"],
-        }
-        merged_list.append(merged_dict)
+#     # Add any remaining sales records to the merged list
+#     for sales_record in sales_dict.values():
+#         merged_dict = {
+#             "sales_target-{}-{}".format(sales_record["month"], sales_record["year"]): sales_record["sales_target"],
+#             "paid_amount-{}-{}".format(sales_record["month"], sales_record["year"]): 0.0,
+#             "variance-{}-{}".format(sales_record["month"], sales_record["year"]): sales_record["sales_target"],
+#             "sales_person": sales_record["sales_person"],
+#             "cost_center": sales_record["cost_center"],
+#         }
+#         merged_list.append(merged_dict)
     
-    return merged_list
+#     return merged_list
 
-def group_by_cost_center(data):
-    result = []
-    cost_centers = set(d['cost_center'] for d in data)
-    for cost_center in cost_centers:
-        target = {}
-        for d in data:
-            if d['cost_center'] == cost_center:
-                target.update(d)
-        result.append(target)
-    return result
+# def group_by_cost_center(data):
+#     result = []
+#     cost_centers = set(d['cost_center'] for d in data)
+#     for cost_center in cost_centers:
+#         target = {}
+#         for d in data:
+#             if d['cost_center'] == cost_center:
+#                 target.update(d)
+#         result.append(target)
+#     return result
 
 
-def get_month_labels(year):
-    import calendar
-    """
-    Returns a list of dictionaries with fieldname and label for each month in the given year.
-    """
-    month_labels = []
-    for month in range(1, 13):
-        month_name = calendar.month_name[month]
-        month_labels.append({
-            "fieldname": f"sales_target-{month}-{year}",
-            "label": f"Target-{month_name}({year})",
-			"type":"float",
-			"width":"80px"
-        })
-        month_labels.append({
-            "fieldname": f"paid_amount-{month}-{year}",
-            "label": f"Achieved-{month_name}({year})",
-			"type":"float",
-			"width":"80px"
-        })
-        month_labels.append({
-            "fieldname": f"variance-{month}-{year}",
-            "label": f"Variance-{month_name}({year})",
-			"type":"float",
-			"width":"80px"
-        })
-    return month_labels
+# def get_month_labels(year):
+#     import calendar
+#     """
+#     Returns a list of dictionaries with fieldname and label for each month in the given year.
+#     """
+#     month_labels = []
+#     for month in range(1, 13):
+#         month_name = calendar.month_name[month]
+#         month_labels.append({
+#             "fieldname": f"sales_target-{month}-{year}",
+#             "label": f"Target-{month_name}({year})",
+# 			"type":"float",
+# 			"width":"80px"
+#         })
+#         month_labels.append({
+#             "fieldname": f"paid_amount-{month}-{year}",
+#             "label": f"Achieved-{month_name}({year})",
+# 			"type":"float",
+# 			"width":"80px"
+#         })
+#         month_labels.append({
+#             "fieldname": f"variance-{month}-{year}",
+#             "label": f"Variance-{month_name}({year})",
+# 			"type":"float",
+# 			"width":"80px"
+#         })
+#     return month_labels
